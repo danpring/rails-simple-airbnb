46daf713c9f008c9a9c62d491691c905986ec90e
Dan Ring <dan.ring@me.com>
2021-01-14 22:01:00 +0900

    attempted airbnb exercise

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database..."
+Flat.destroy_all
+
+puts "Creating flats..."
+5.times do
+  Flat.create!(
+    name: "A nice #{["house", "dungeon", "palace", "apartment", "bedsit" ].sample} located in #{["Singapore", "Tokyo", "London", "New York", "New Orleans"].sample}.",
+    address: Faker::Address.street_address,
+    description: Faker::Lorem.paragraph_by_chars,
+    price_per_night: rand(7_000..15_000),
+    number_of_guests: rand(2..8)
+)
+end
+puts "done!"
